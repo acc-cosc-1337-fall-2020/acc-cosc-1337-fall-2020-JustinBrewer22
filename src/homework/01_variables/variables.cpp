c85d69dda69133cc0c6efc7691851c3de72625a3
Justin Brewer <justin@Justins-MacBook-Air.local>
2020-12-09 06:13:45 -0600

    Homework 1

--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -8,7 +8,19 @@ int add_numbers(int num1, int num2)
 
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
+const double tax_rate = 0.0675;
 
+double get_sales_tax_amount (double meal_amount)
+{
+	double meal_w_tax = (meal_amount * tax_rate);
+	return meal_w_tax;
+}
+
+double get_tip_amount (double meal_amount, double tip_rate)
+{
+	double meal_w_tip = (meal_amount * tip_rate)/100;
+	return meal_w_tip;
+}
 
 
 
